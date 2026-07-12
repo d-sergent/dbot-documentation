e0f010f80aee6b1bc51a52611a921b23b1777612
David Sergent <email@davidsergent.com>
2026-07-12 22:05:48 +0200

    code: temporarily modify test_neck.py to test Tilt ID 2 backward by 10 degrees

--- code/scripts/motors/test_neck.py
+++ code/scripts/motors/test_neck.py
@@ -16,9 +16,7 @@ from dbot.motors.neck import NeckController
 
 SEQUENCE = [
     ("Centre",        0,   0, 1.5),
-    ("Pan +80°",     80,   0, 9.0),
-    ("Centre",        0,   0, 1.5),
-    ("Pan -80°",    -80,   0, 9.0),
+    ("Tilt -10° (Arrière)", 0,  -10, 2.5),
     ("Centre final",  0,   0, 1.5),
 ]
 
