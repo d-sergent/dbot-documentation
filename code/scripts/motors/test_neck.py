c91a399b17e5ce553bf0dd6765b99284687af86d
David Sergent <email@davidsergent.com>
2026-07-19 08:49:21 +0200

    MAJ test_neck.py avec amplitude Tilt à ±15°

--- code/scripts/motors/test_neck.py
+++ code/scripts/motors/test_neck.py
@@ -16,9 +16,9 @@ from dbot.motors.neck import NeckController
 
 SEQUENCE = [
     ("Centre",        0,   0, 1.5),
-    ("Tilt +10°",     0,  10, 4.0),
+    ("Tilt +15°",     0,  15, 4.0),
     ("Centre",        0,   0, 1.5),
-    ("Tilt -10°",     0, -10, 4.0),
+    ("Tilt -15°",     0, -15, 4.0),
     ("Centre final",  0,   0, 1.5),
 ]
 
